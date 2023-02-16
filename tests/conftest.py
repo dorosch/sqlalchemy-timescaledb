b8c669cadfb6bf7d5f7d5f441be89736f82dea28
Andrey Kletsko <andrey.daraschenka@gmail.com>
2023-02-16 14:45:31 +0200

    Change docker-compose.yml port for timescaledb

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,49 +4,48 @@ import pytest
 from sqlalchemy import create_engine, text
 from sqlalchemy.dialects import registry
 from sqlalchemy.engine import URL
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 
 from tests.models import Base
 
 
-def pytest_configure(config):
-    path = 'sqlalchemy_timescaledb.dialect'
+registry.register(
+    'timescaledb',
+    'sqlalchemy_timescaledb.dialect',
+    'TimescaledbPsycopg2Dialect'
+)
+registry.register(
+    'timescaledb.psycopg2',
+    'sqlalchemy_timescaledb.dialect',
+    'TimescaledbPsycopg2Dialect'
+)
+registry.register(
+    'timescaledb.asyncpg',
+    'sqlalchemy_timescaledb.dialect',
+    'TimescaledbAsyncpgDialect'
+)
 
-    registry.register('timescaledb', path, 'TimescaledbPsycopg2Dialect')
-    registry.register('timescaledb.psycopg2', path, 'TimescaledbPsycopg2Dialect')
-    registry.register('timescaledb.asyncpg', path, 'TimescaledbAsyncpgDialect')
-
-
-@pytest.fixture(scope='module')
-def engine(
-        host='0.0.0.0',
-        port=8001,
-        user='user',
-        password='password',
-        database='database',
-        driver='timescaledb'
-):
-    return create_engine(
-        URL.create(
-            host=os.environ.get('POSTGRES_HOST', host),
-            port=os.environ.get('POSTGRES_PORT', port),
-            username=os.environ.get('POSTGRES_USER', user),
-            password=os.environ.get('POSTGRES_PASSWORD', password),
-            database=os.environ.get('POSTGRES_DB', database),
-            drivername=driver
-        )
+engine = create_engine(
+    URL.create(
+        host=os.environ.get('POSTGRES_HOST', '0.0.0.0'),
+        port=os.environ.get('POSTGRES_PORT', 5432),
+        username=os.environ.get('POSTGRES_USER', 'user'),
+        password=os.environ.get('POSTGRES_PASSWORD', 'password'),
+        database=os.environ.get('POSTGRES_DB', 'database'),
+        drivername=os.environ.get('DRIVERNAME', 'timescaledb')
     )
+)
+Session = scoped_session(sessionmaker(bind=engine))
 
 
-@pytest.fixture(scope='module')
-def session(engine):
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    db_session = Session()
-    yield db_session
-    db_session.rollback()
-    db_session.close()
+def pytest_configure(config):
     Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+
+
+@pytest.fixture
+def session():
+    yield Session
 
 
 @pytest.fixture
