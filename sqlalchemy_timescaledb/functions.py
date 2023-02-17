1728156918b24dda20381e46ce1afb35822972af
Andrey Kletsko <andrey.daraschenka@gmail.com>
2023-02-17 18:37:59 +0200

    Added last() function and tests for it

--- sqlalchemy_timescaledb/functions.py
+++ sqlalchemy_timescaledb/functions.py
@@ -4,3 +4,8 @@ from sqlalchemy.sql.functions import GenericFunction
 class First(GenericFunction):
     identifier = 'first'
     inherit_cache = True
+
+
+class Last(GenericFunction):
+    identifier = 'last'
+    inherit_cache = True
